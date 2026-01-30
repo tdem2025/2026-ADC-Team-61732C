6604a61751edd26a18cb2b494d66fbea367d6b3e
tdem2025 <tdemirchyan2010@gmail.com>
2026-01-29 20:07:38 -0800

    We coded, yes

--- autonomous.py
+++ autonomous.py
@@ -5,6 +5,23 @@ drone.pair()
 
 drone.takeoff()
 
+drone.move_distance(0, 0, 0.9,1)
+#drone.hover(5)
+#drone.set_yaw(0)
+#drone.move_forward(distance=1.7, units="m", speed=1)
+drone.flip("back")
+drone.hover(2)
+drone.flip("back")
+drone.hover(2)
+drone.flip("back")
+drone.hover(2)
+drone.flip("back")
+drone.hover(2)
+drone.flip("back")
+drone.hover(2)
+
+
+
 drone.land()
 
 drone.close()
